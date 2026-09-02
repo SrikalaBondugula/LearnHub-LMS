7da9089717c404b7855f3c57c1317b141b3a302a
SrikalaBondugula <srikalabondugula08@gmail.com>
2026-09-02 22:58:41 +0530

    Fix allowed hosts for Render

--- lms_project/lms_project/settings.py
+++ lms_project/lms_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['learnhub-lms-sfg0.onrender.com','localhost']
+ALLOWED_HOSTS = ['learnhub-lms-sfg0.onrender.com','localhost', "127.0.0.1"]
 
 
 # Application definition
